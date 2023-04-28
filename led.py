b12c63cf3978de62f8e79cd59c9b8f59b60a035e
kumar <kishorekumarsaran2001@gmail.com>
2023-04-28 16:02:49 +0530

    Trying GPIO program in RPI library

---
+++ led.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+while True:
+    print("[] Turning ON LED...")
+    GPIO.output(17, GPIO.HIGH)
+    sleep(1)
+    print("[] Turning OFF LED...")
+    GPIO.output(17, GPIO.LOW)
+    sleep(1)
